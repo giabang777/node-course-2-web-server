e76c90d05e4a95b10764f4263b43ef60b79fcb1b
giabang777 <giabang777@gmail.com>
2017-09-22 11:19:17 +0700

    Add start script and change PORT for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 // app.use(express.static(__dirname+'/public'));
@@ -54,6 +55,6 @@ app.get('/bad', (req, res) => {
   })
 });
 
-app.listen('3000', () => {
-  console.log("server is up on 3000 port");
+app.listen(port, () => {
+  console.log(`server is up on ${port} port`);
 });
